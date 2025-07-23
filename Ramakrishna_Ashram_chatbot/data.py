0efc687b93cc086059c7b554f11379620df916e2
ethanhunt123vvv <123399255+ethanhunt123vvv@users.noreply.github.com>
2025-07-23 17:11:25 +0530

    Update data.py

--- Ramakrishna_Ashram_chatbot/data.py
+++ Ramakrishna_Ashram_chatbot/data.py
@@ -2,12 +2,18 @@ from langchain_community.document_loaders import DirectoryLoader, PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from langchain_pinecone import PineconeVectorStore
 from dotenv import load_dotenv
-import os
+import os,pinecone
 
 load_dotenv()
-qdrant_key = os.getenv("QDRANT_KEY")
-qdrant_url = os.getenv("QDRANT_URL")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+pinecone_env = os.getenv("PINECONE_ENV")  
+index_name = "centenary-celebration"
+
+
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
 
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size = 1000,
@@ -24,12 +30,9 @@ for doc in docs:
 split_docs = text_splitter.split_documents(docs)
 split_docs = [doc for doc in split_docs if len(doc.page_content.strip())>30]
 
-vector_store = QdrantVectorStore.from_documents(
-    split_docs,
-    OpenAIEmbeddings(model="text-embedding-3-small"),
-    url = qdrant_url,
-    api_key = qdrant_key,
-    batch_size = 31,
-    collection_name= "Centanary_Celebrations"
+vector_store = PineconeVectorStore.from_documents(
+    documents = split_docs,
+    embeddings = embeddings,
+    index_name = index_name
 )
 
